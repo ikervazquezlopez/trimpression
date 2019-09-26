d614860fd94057b3d17690ec8b012266ef554f3f
Iker Vazquez Lopez <ikervazquezlopez@u.boisestate.edu>
2019-09-26 09:17:18 -0600

    Added functionality to determine if a point is inside a circle

--- Trimpression/utils.cpp
+++ Trimpression/utils.cpp
@@ -1,29 +1,53 @@
 #include "utils.h"
 #include "global.h"
+#include <iostream>
 
+
+// Computes the determinant of a 4x4 matrix.
+float determinant(float matrix[4][4], int n=4) {
+	float det = 0;
+	float submatrix[4][4];
+	if (n == 2)
+		return ((matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
+	else {
+		for (int x = 0; x < n; x++) {
+			int subi = 0;
+			for (int i = 1; i < n; i++) {
+				int subj = 0;
+				for (int j = 0; j < n; j++) {
+					if (j == x)
+						continue;
+					submatrix[subi][subj] = matrix[i][j];
+					subj++;
+				}
+				subi++;
+			}
+			det = det + (pow(-1, x) * matrix[0][x] * determinant(submatrix, n - 1));
+		}
+	}
+	return det;
+}
+
+
+// Determines if point p is inside the circle defined by the triangle p0-p1-p2 (counterclockwise order)
 bool point_in_circle(Trimpression_Mesh::Point p, 
 						Trimpression_Mesh::Point p0, 
 						Trimpression_Mesh::Point p1, 
 						Trimpression_Mesh::Point p2)
 {
-	// Compute constant values in the formula
-	float _2x10 = 2 * p1[0] - 2 * p0[0];
-	float _2x20 = 2 * p2[0] - 2 * p0[0];
-	float _2y10 = 2 * p1[1] - 2 * p0[1];
-	float _2y20 = 2 * p2[1] - 2 * p0[1];
-	float _px01 = p0[0] * p0[0] - p1[0] * p1[0];
-	float _px02 = p0[0] * p0[0] - p2[0] * p2[0];
-	float _py01 = p0[1] * p0[1] - p1[1] * p1[1];
-	float _py02 = p0[1] * p0[1] - p2[1] * p2[1];
+	float matrix[4][4];
 
-	// Compute circle center and radius
-	float y_c = (-_px02 - _py02) / (_2x20 * (_2y10 + _px01 + _py01) / _2x10 + _2y20);
-	float x_c = (y_c * _2y10 + _px01 + _py01) / _2x10;
-	double r = sqrt((p0[0] - x_c) * (p0[0] - x_c) + (p0[1] - y_c) * (p0[1] - y_c));
+	matrix[0][0] = p0[0]; matrix[0][1] = p0[1]; matrix[0][2] = p0[0] * p0[0] + p0[1] * p0[1];
+	matrix[1][0] = p1[0]; matrix[1][1] = p1[1]; matrix[1][2] = p1[0] * p1[0] + p1[1] * p1[1];
+	matrix[2][0] = p2[0]; matrix[2][1] = p2[1]; matrix[2][2] = p2[0] * p2[0] + p2[1] * p2[1];
+	matrix[3][0] = p[0]; matrix[3][1] = p[1]; matrix[3][2] = p[0] * p[0] + p[1] * p[1];
+	
+	matrix[0][3] = 1;
+	matrix[1][3] = 1;
+	matrix[2][3] = 1;
+	matrix[3][3] = 1;
 
-	// Compute distance from circle center to point
-	double d = sqrt((p[0] - x_c) * (p[0] - x_c) + (p[1] - y_c) * (p[1] - y_c));
-
-	// Evaluate if the point is inside the circle
-	return d < r;
+	float det = determinant(matrix);
+	
+	return det >= 0;
 }
--- Trimpression/utils.h
+++ Trimpression/utils.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "global.h"
 
+float determinant(float matrix[4][4], int n);
+
 bool point_in_circle(Trimpression_Mesh::Point p, 
 						Trimpression_Mesh::Point p0, 
 						Trimpression_Mesh::Point p1, 
